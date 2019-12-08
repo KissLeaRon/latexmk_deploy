f2826d9942fd3295511cb63060fcaf6e720cc9e2
Kota <CyaleaKiss@gmail.com>
2019-12-09 05:49:19 +0900

    pdf

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,7 +18,7 @@ echo "<!DOCTYPE HTML>
 echo "Building $directory"
 cd $directory
 mkdir -p $OUT_DIR/$directory
-latexmk -pdf -output-directory=$OUT_DIR/$directory
+latexmk -output-directory=$OUT_DIR/$directory
 # Remove every output that isn't a PDF
 find $OUT_DIR/$directory -type f ! -name "*.pdf" -exec rm {} \;
 FILENAME=$(find $OUT_DIR/$directory -type f -name "*.pdf")
